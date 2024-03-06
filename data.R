7bf464e62ad832fc9ed283874f1a0823fa7c498f
Hafeezul Raziq <hafeezul.rzq@gmail.com>
2024-03-06 22:46:02 +0800

    Update

--- data.R
+++ data.R
@@ -10,6 +10,7 @@ fr_sf <- st_read("~/Documents/github_local/forestreserve_bn/data/fr_map.shp")
 # Check fr_sf
 p1 <- ggplot(dis_sf) +
   geom_sf() +
-  geom_sf(data = fr_sf, fill = "green4")
+  geom_sf(data = fr_sf, fill = "green4") +
+  labs(title = 'Gazetted Forest Reserves of Brunei Darussalam')
 
 ggsave("forestreserve_bn.png", plot = p1)
