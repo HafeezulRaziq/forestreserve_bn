c88309b3515616352cb286439d5e9d6d06fdb105
Hafeezul Raziq <hafeezul.rzq@gmail.com>
2024-03-07 11:48:13 +0800

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Plot
+
+![](forestreserve_bn.png)
+
+# Acknowledgements
+
+![](forestreserve_bn_ref.png) The map was referenced from THE 5TH NATIONAL REPORT TO THE CONVENTION ON BIOLOGICAL DIVERSITY by The Forestry Department, Ministry of Industry and Primary Resources.
--- data.R
+++ data.R
@@ -11,6 +11,7 @@ fr_sf <- st_read("~/Documents/github_local/forestreserve_bn/data/fr_map.shp")
 p1 <- ggplot(dis_sf) +
   geom_sf() +
   geom_sf(data = fr_sf, fill = "green4") +
-  labs(title = 'Gazetted Forest Reserves of Brunei Darussalam')
+  labs(title = 'Gazetted Forest Reserves of Brunei Darussalam')+
+  theme(plot.title = element_text(size = 20))
 
 ggsave("forestreserve_bn.png", plot = p1)
